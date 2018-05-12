e07d41e03ef4e959c3444abaf98c1c427236ee5e
Björn Friedrichs <bjoern@friedrichs1.de>
2018-05-12 17:56:34 +0200

    use the aiohttp_session package keys for the middleware

--- tfschmuck/server/middlewares/session.py
+++ tfschmuck/server/middlewares/session.py
@@ -1,20 +1,18 @@
 from aiohttp import web
-
-SESSION_KEY = 'aiohttp_session'
-STORAGE_KEY = 'aiohttp_session_storage'
+import aiohttp_session
 
 
 def middleware(storage):
     @web.middleware
     async def factory(request, handler):
-        request[STORAGE_KEY] = storage
+        request[aiohttp_session.STORAGE_KEY] = storage
         raise_response = False
         try:
             response = await handler(request)
         except web.HTTPException as exc:
             response = exc
             raise_response = True
-        session = request.get(SESSION_KEY)
+        session = request.get(aiohttp_session.SESSION_KEY)
         if session is not None:
             if session._changed:
                 await storage.save_session(request, response, session)
